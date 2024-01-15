5937ba75545655829c46e4e2432473223bb05cc2
Puteri Yudani <puteriyudani@gmail.com>
2024-01-15 12:29:42 +0700

    ingatkan saya hidden

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,12 +64,12 @@
                                         </div>
                                     </div>
 
-                                    <div class="form-group">
+                                    {{-- <div class="form-group">
                                         <div class="custom-control custom-checkbox">
                                             <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
                                             <label class="custom-control-label" for="remember-me">Ingatkan Saya</label>
                                         </div>
-                                    </div>
+                                    </div> --}}
 
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
